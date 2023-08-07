3e5277b9bbaa213db1e6048df6522f66d2f7127a
amine <boukhrisamine210@gmail.com>
2023-08-07 22:06:47 +0100

    error handling styles

---
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -0,0 +1,37 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const email = form.querySelector("#email");
+  if (!isValidEmail(email.value)) {
+    const errorMessage = form.querySelector(".form__error-message");
+    errorMessage.classList.add("error");
+    email.classList.add("error");
+    return;
+  }
+
+  try {
+    const response = await fetch("url", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email.value }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log("Email added successfully:", data);
+    } else {
+      console.error("Error adding email:", response.statusText);
+    }
+  } catch (error) {
+    console.error("An error occurred while adding email:", error);
+  }
+});
+
+function isValidEmail(email) {
+  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return pattern.test(email);
+}
