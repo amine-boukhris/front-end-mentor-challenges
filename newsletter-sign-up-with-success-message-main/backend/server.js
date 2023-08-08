3dd7da47852e10cbf450c5c910daf3d8894c5449
amine <boukhrisamine210@gmail.com>
2023-08-08 10:09:31 +0100

    sending and handling request

---
+++ newsletter-sign-up-with-success-message-main/backend/server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const mongodb = require("mongodb");
+const { MongoClient } = mongodb;
+const asyncHandler = require("express-async-handler");
+const cors = require("cors");
+
+const uri = "mongodb+srv://boukhrisamine:mino2007@aminecluster.xtawsm6.mongodb.net/";
+const databaseName = "newsteller";
+const collectionName = "subscribers_emails";
+
+let client;
+
+async function connectToMongoDB() {
+  try {
+    client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    console.log("Connected to database successfully.");
+  } catch (error) {
+    console.log("error connection to database:", error);
+    throw error;
+  }
+}
+
+connectToMongoDB();
+
+const port = 3000;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+app.post(
+  "/subscribe",
+  asyncHandler(async (req, res) => {
+    const collection = client.db(databaseName).collection(collectionName);
+    const data = req.body;
+    const emailExist = await collection.findOne({ email: data.email });
+    if (emailExist) {
+      res.status(409);
+      throw new Error("email already subscribed.");
+    }
+    await collection.insertOne(data);
+    res.send("Email data inserted successfully.");
+  })
+);
+
+app.listen(port, () => {
+  console.log("listening on port: ", port);
+});
+
+// Close the database connection on application termination
+// process.on("SIGINT", () => {
+//   client.close(() => {
+//     console.log("MongoDB connection closed.");
+//     process.exit(0);
+//   });
+// });
