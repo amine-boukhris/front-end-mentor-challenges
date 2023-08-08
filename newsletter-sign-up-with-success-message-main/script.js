38fc0850330c108d92ba527a50440248867706cc
amine <boukhrisamine210@gmail.com>
2023-08-08 11:01:36 +0100

    handling changes between states

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -1,11 +1,16 @@
 const form = document.querySelector("form");
+const dismiss_button = document.querySelector(".success__form-button");
+
+const form_image = document.querySelector(".form__image");
+const form_content = document.querySelector(".form__content__container");
+const success_content = document.querySelector(".success");
+const errorMessage = form.querySelector(".form__error-message");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const email = form.querySelector("#email");
   if (!isValidEmail(email.value)) {
-    const errorMessage = form.querySelector(".form__error-message");
     errorMessage.classList.add("error");
     email.classList.add("error");
     return;
@@ -22,13 +27,28 @@ form.addEventListener("submit", async (event) => {
 
     if (response.ok) {
       const data = await response.text();
-      console.log("Email added successfully:", data);
+      form_image.classList.add("none");
+      form_content.classList.add("none");
+      success_content.classList.remove("none");
+      errorMessage.classList.add("error");
+
+      email.value = "";
+      // console.log("Email added successfully:", data);
     } else {
       console.error("Error adding email:", response.statusText);
+      email.classList.add("error");
     }
   } catch (error) {
     console.error("An error occurred while adding email:", error);
   }
+});
+
+dismiss_button.addEventListener("click", () => {
+  form_image.classList.remove("none");
+  form_content.classList.remove("none");
+  success_content.classList.add("none");
+  errorMessage.classList.remove("error");
+  email.classList.remove("error");
 });
 
 function isValidEmail(email) {
